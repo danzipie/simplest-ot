72028898faa6778107ecf82b72f8f7a59434935b
danzipie <danzipie@gmail.com>
2019-08-07 18:00:31 +0200

    decrypts the correct message

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # simplest-ot
-Implementation of Chou-Orlandi Oblivious Transfer protocol
+Implementation of Chou-Orlandi Oblivious Transfer (OT) protocol
+
+## Work in progress
+
+A n-to-1 OT protocol practical implementation in Rust, as thin as possible.
+The purpose is to learn how to use Rust crypto libraries, not to produce a tool.
+
+## Quickstart
+
+```
+cargo build
+./target/debug/simplest-ot
+```
+
+## Design
+
+It uses Chou-Orlandi "Simplest OT" protocol to establish key exchange, and AES encryption for the actual information.
+
+## References
+
+OT protocol article:
+- https://eprint.iacr.org/eprint-bin/getfile.pl?entry=2015/267&version=20180529:135402&file=267.pdf
+
+Another implemenration of the Simplest OT is here:
+- https://github.com/GaloisInc/ocelot/
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use ring::digest;
 use ring::rand::{SecureRandom, SystemRandom};
 
 /**
- * Oblivious Transfer sender and receiver
- * based on Chou-Orlandi OT
- * https://eprint.iacr.org/eprint-bin/getfile.pl?entry=2015/267&version=20180529:135402&file=267.pdf
+ * Oblivious Transfer sender and receiver based on Chou-Orlandi OT
  */
 
 pub struct Sender {
@@ -28,13 +26,13 @@ pub struct Receiver {
 
 impl Sender {
 
+    // construct a Sender
     fn new(n: u32, b: RistrettoBasepointTable) -> Sender {
-        println!("Initialize y");
         let rng = SystemRandom::new();
         Sender { n: n, b: b, rng: rng, y: Scalar::one() }
     }
 
-    // output S = y * B
+    // select private parameter y and return S = y * B
     fn setup(&mut self) -> RistrettoPoint {
         let mut random_value = [0; 32];
         self.rng.fill(&mut random_value).unwrap();
@@ -43,10 +41,10 @@ impl Sender {
         &y * &self.b
     }
 
+    // derive and return the n encryption keys
     fn derive_keys(&mut self, s: RistrettoPoint, r: RistrettoPoint) -> Vec<ring::digest::Digest> {
         // @TODO check if r in field
         let t = &self.y * s;
-        //let k: [<u8>, n];
         let mut k = Vec::new();
         for idx in 0u32..self.n {
             let j = Scalar::from(idx);
@@ -57,27 +55,22 @@ impl Sender {
         k
     }
 
-    fn encrypt(&self, k_s: Vec<ring::digest::Digest>) -> Vec<Vec<u8>> {
+    fn encrypt(&self, k_s: Vec<ring::digest::Digest>, messages: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
         let mut in_out = Vec::new();
-        print!("encrypt function ");
-        println!("{:?}", k_s.len());
         for p in 0..k_s.len() { // encrypt each message with different key
             let k = k_s[p].as_ref();
-            print!("Index: ");
-            println!("{:?}", p);
             let key = LessSafeKey::new(UnboundKey::new(&AES_256_GCM, &k).unwrap());
             let nonce_bytes = [0; 12]; // todo: generate random
             let nonce = Nonce::assume_unique_for_key(nonce_bytes);
             print!("Nonce is: ");
             println!("{:?}", nonce_bytes);
-            let content = b"content to encrypt".to_vec();
-            let in_out_t = content.clone();
+            let in_out_t = messages[p].clone();
             in_out.push(in_out_t);
             print!("Encrypting: ");
             println!("{:?}", in_out[p]);
-            let _len = key.seal_in_place(nonce, Aad::empty(), &mut in_out[p]).unwrap();
+            let _len = key.seal_in_place_append_tag(nonce, Aad::empty(), &mut in_out[p]).unwrap();
             print!("Encrypted: ");
-            println!("{:?}", content);
+            println!("{:?}", in_out[p]);
         }
         in_out
     }
@@ -86,14 +79,13 @@ impl Sender {
 impl Receiver {
 
     fn new(n: u32, b: RistrettoBasepointTable) -> Receiver {
-        println!("Initialize receiver");
         let rng = SystemRandom::new();
         let k = digest::digest(&digest::SHA256, b"");
         let key = LessSafeKey::new(UnboundKey::new(&AES_256_GCM, &[0u8; 32]).unwrap());
         Receiver { n: n, b: b, rng: rng, k: k, key: key}
     }
 
-    // returns the encryption key 'k'
+    // returns the encryption key 'k' based on secret parameter x and external param S
     fn choose(&mut self, c: Scalar, s: RistrettoPoint) -> RistrettoPoint {
         let random_value = [0; 32];
         let x = Scalar::from_bits(random_value); // sample random value
@@ -101,19 +93,19 @@ impl Receiver {
         let args = &x * s;
         let args = args.compress();
         self.k = digest::digest(&digest::SHA256, args.as_bytes()); // key derivation
+        self.key = LessSafeKey::new(UnboundKey::new(&AES_256_GCM, &self.k.as_ref()).unwrap());
         r
     }
 
+    // decrypts all messages. only one is expected to succeed
     fn decrypt(&self, mut in_out: Vec<Vec<u8>>) {
-        println!("Decrypt function");
         for p in 0..in_out.len() {
-            let mut nonce_bytes = [0; 12]; // todo: generate random
-            let mut nonce_r = Nonce::assume_unique_for_key(nonce_bytes);
-            print!("Decrypting ");
-            println!("{:?}", in_out[p]);
-            let res = self.key.open_in_place(nonce_r, Aad::empty(), &mut in_out[p]).unwrap();
-            print!("Decrypted ");
-            println!("{:?}", res);
+            let nonce_bytes = [0; 12]; // todo: generate random
+            let nonce_r = Nonce::assume_unique_for_key(nonce_bytes);
+            match self.key.open_in_place(nonce_r, Aad::empty(), &mut in_out[p]) {
+                Ok(v) => println!("{:?}", v),
+                Err(e) => println!("Impossible to decrypt")
+            }
         }
     }
 
@@ -123,26 +115,22 @@ fn main() {
 
     println!("Initiating protocol");
 
-    const n: u32 = 2; // number of alternative messages
-    const l: u32 = 128;
+    const N: u32 = 2; // number of alternative messages
+    const L: u32 = 128;
 
-    let mut sender = Sender::new(n, RISTRETTO_BASEPOINT_TABLE);
+    let mut sender = Sender::new(N, RISTRETTO_BASEPOINT_TABLE);
     let s = sender.setup();
-    // println!("{:?}", s);
 
     // S --- s ---> R
     let c = Scalar::one(); // choose which info to be received ( @TODO: input of the program)
-    let mut receiver = Receiver::new(n, RISTRETTO_BASEPOINT_TABLE);
+    let mut receiver = Receiver::new(N, RISTRETTO_BASEPOINT_TABLE);
     let r = receiver.choose(c, s);
-    println!("choose");
 
     // R --- r ---> S
     let k_s = sender.derive_keys(s, r);
-    println!("derived keys");
-    // k_s is a vector of keys
 
     // transfer phase
-    let encrypted_message = sender.encrypt(k_s);
-    println!("encrypted");
-    receiver.decrypt(encrypted_message);
+    let messages = vec![b"one".to_vec(), b"two".to_vec()];
+    let encrypted_messages = sender.encrypt(k_s, messages);
+    receiver.decrypt(encrypted_messages);
 }
